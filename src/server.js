dc96c5595a9ef39cae90d0f492d93a6fe1e8364e
spacewave1 <w.nabok@gmail.com>
2020-10-30 02:57:31 +0100

    Heroku port

--- src/server.js
+++ src/server.js
@@ -183,6 +183,6 @@ app.get('/api/relationship/list', (req, res) => {
 
 
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+app.listen(process.env.PORT || port, () => {
+  console.log(`Example app listening at http://localhost:${port} or ${process.env.PORT}`)
 })
