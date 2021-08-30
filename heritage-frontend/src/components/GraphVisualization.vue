c231b777256e62fc2464c89327da06cd653edbfd
spacewave1 <w.nabok@gmail.com>
2021-08-30 02:08:13 +0200

    Convert frontend branch structure into levels

--- heritage-frontend/src/components/GraphVisualization.vue
+++ heritage-frontend/src/components/GraphVisualization.vue
@@ -1,13 +1,15 @@
 <template>
 	<div>
-		<v-simple-table>
-			<tbody>
+		<tbody>
 				<tr v-for="branch in branches" :key="branch.id">
 					<branch :branchData="branch"></branch>
 				</tr>
-				
-			</tbody>
-		</v-simple-table>
+			<br>
+			<div v-for="item in Object.entries(levels)" :key="item.key">
+				{{ item }}
+			</div>
+			<br>
+		</tbody>
 	</div>
 </template>
 
@@ -25,7 +27,9 @@
 				mainBranch: {},
 				secondBranch: {},
 				id: 0,
-				tree:{nodes:[], links:[]}
+				tree:{nodes:[], links:[]},
+				levels: new Map(),
+				xMax: 0
 			}
 		},
 		components: {
@@ -33,43 +37,40 @@
 		},
 		created() {
 			this.getBranches().then(data => {
-				//console.log(data)
-				let index = 0
-				this.branches = data.branches.map(entry => {
-					entry.path = JSON.parse(entry.path)
-					index+=1
-					entry.id = index
-					return entry
-				})
-
-				let xIndex = 0
-
+				
 				this.tree = {nodes:[], links:[]}
-				console.log(this.tree)
+				this.branches = data.branches.sort((b1, b2) => b2.l - b1.l)
 
-				this.branches.forEach(branch => {
-					let processedBranch = this.buildRecursiveTreeFromBranch(branch.path, branch.path.child, this.tree.nodes, this.tree.links, xIndex)
-					this.treeNodes = processedBranch.treeNodes
-					this.treeLinks = processedBranch.treeLinks
-					xIndex = processedBranch.xIndex
-					console.log(processedBranch)
+				let index = 0
+				let processedBranches = []
+
+				this.branches = this.branches.map(branch => {
+					branch.path = JSON.parse(branch.path)
+					const offset = this.calculateYOffset(branch.path, processedBranches, 0)
+					branch.levels = this.calculateYPositions(branch.path, [], offset)
+					processedBranches = processedBranches.map(procBranch => {
+						if(branch.levels.map(pEntry => pEntry.name).includes(procBranch.name)){
+							procBranch.parents = procBranch.parents.concat(...branch.levels.filter(lBranch => lBranch.name === procBranch.name && !lBranch.parents.includes(...procBranch.parents)).map(lBranch => lBranch.parents))
+							return procBranch
+						}
+						return procBranch
+
+					})
+					processedBranches.push(...branch.levels.filter(entry => !processedBranches.map(pEntry => pEntry.name).includes(entry.name)))
+					index+=1
+					branch.id = index
+					return branch
 				})
 
-				let max = Math.max(...this.branches.map(branch => branch.l))
-				let otherMaxBranches = []
-				let mainBranchCandidates = this.branches.filter(branch => branch.l === max)
+				index = 0
 
-				this.mainBranch = mainBranchCandidates[0]
-				this.renderedNodes = this.getRecursiveNames(this.mainBranch.path, [], 0)
-				//console.log(this.renderedNodes)
-				let restOfBranches = this.branches.filter(branch => branch.l !== max)
+				let max = Math.max(...processedBranches.map(procBranch => procBranch.y))
 
-				if(mainBranchCandidates.Length > 1){
-					otherMaxBranches = mainBranchCandidates.shift()
-					restOfBranches.concat(otherMaxBranches)
+				while(index <= max) {
+					this.levels[index] = processedBranches.filter(procBranch => procBranch.y === index)
+					index += 1
 				}
-
-				//console.log(restOfBranches.map(branch => this.getRecursiveNames(branch.path, [], 0)).filter(branch => branch.map(node => this.renderedNodes.map(ren => ren.name).includes(node.name)).reduce((a,b) => a || b)))
+				this.xMax = Math.max(...Object.values(this.levels).map(level => level.length))
 				
 			})
 		},
@@ -83,42 +84,39 @@
 					return this.getRecursiveNames(tail.child, nodes, index)
 				}
 			},
-			// TODO: Debug Position in Layout when duplicate, also debug homer on same level like bart
-			buildRecursiveTreeFromBranch(parentNode, childNode, treeNodes, treeLinks, xIndex){
-				if(childNode === undefined){
-					return({treeNodes:treeNodes, treeLinks:treeLinks, xIndex:xIndex})
-				} else {
-					if(treeLinks.filter(link => link.parent === (parentNode.forname + parentNode.surname) && link.child === (childNode.forname + childNode.surname)).length > 0){
-						return this.buildRecursiveTreeFromBranch(childNode, childNode.child, treeNodes, treeLinks, xIndex)
-					} else {
-						if(treeLinks.filter(link => link.parent === (parentNode.forname + parentNode.surname)).length === 1) {
-							let existingParent = treeNodes.filter(node => node.name === (parentNode.forname + parentNode.surname))[0]
-							treeLinks.push({parent: existingParent.name, child: childNode.forname + childNode.surname})
-							if(treeNodes.filter(node => node.name === childNode.forname + childNode.surname).length === 0){
-								treeNodes.push({name: childNode.forname + childNode.surname, position: {x: existingParent.position.x, y:existingParent.position.y + 1}})
-							}
-
-						} else if(treeLinks.filter(link => link.child === (childNode.forname + childNode.surname)).length === 1){
-							let existingChild = treeNodes.filter(node => node.name === (childNode.forname + childNode.surname))[0]
-							treeLinks.push({parent: parentNode.forname + parentNode.surname, child: existingChild.name})
-							if(treeNodes.filter(node => node.name === parentNode.forname + parentNode.surname).length === 0){
-								treeNodes.push({name: parentNode.forname + parentNode.surname, position: {x: existingChild.position.x, y:existingChild.position.y - 1}})
-							}
-							
-						} else {
-							treeLinks.push({parent: parentNode.forname + parentNode.surname, child: childNode.forname + childNode.surname})
-							if(treeNodes.filter(node => node.name === parentNode.forname + parentNode.surname).length === 0){
-								treeNodes.push({name: parentNode.forname + parentNode.surname, position: {x: xIndex, y: 0}})
-							}
-							if(treeNodes.filter(node => node.name === childNode.forname + childNode.surname).length === 0){
-								treeNodes.push({name: childNode.forname + childNode.surname, position: {x: xIndex, y: 1}})
-								xIndex += 1
-							}
-						}
-						return this.buildRecursiveTreeFromBranch(childNode, childNode.child, treeNodes, treeLinks, xIndex)
-					}
+			calculateYPositions(entry, returnList, index, parentName){
+				const name = entry.forname + " " + entry.surname
+				returnList.push({name: name, y: index, parents: [parentName]})
+				index += 1
+				if(entry.child !== undefined){
+					return this.calculateYPositions(entry.child, returnList, index, name)
 				}
-			}
+				return returnList
+			},
+			calculateYOffset(branch, processedBranches, index){
+				const indexOf = processedBranches.map(pEntry => pEntry.name).indexOf(branch.forname + " " + branch.surname)
+
+				if(indexOf === -1){
+					if(branch.child === undefined){
+						return 0
+					} else {
+						index += 1
+						return this.calculateYOffset(branch.child, processedBranches, index)
+					}
+				} else {
+					return processedBranches[indexOf].y - index
+				}
+			},
+			checkNameExistsInBranchRecursive(branch, checkName){
+				if(branch.child === undefined) {
+					return false
+				} else if((branch.forname + branch.surname) === checkName){
+					return true
+				} else {
+					return this.checkNameExistsInBranchRecursive(branch.child)
+				}
+			},
+			// TODO: Debug Position in Layout when duplicate, also debug homer on same level like bart
 		}
 	}
 </script>
