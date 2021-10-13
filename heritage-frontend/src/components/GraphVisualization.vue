d40a88d007bec9dccb6f228516069c4c6c30e4b1
spacewave1 <w.nabok@gmail.com>
2021-10-14 00:17:04 +0200

    First biological Tree render functional

--- heritage-frontend/src/components/GraphVisualization.vue
+++ heritage-frontend/src/components/GraphVisualization.vue
@@ -1,15 +1,22 @@
 <template>
 	<div>
-		<tbody>
+		<table id="tree">
+			<tbody>
 				<tr v-for="branch in branches" :key="branch.id">
 					<branch :branchData="branch"></branch>
 				</tr>
-			<br>
-			<div v-for="item in Object.entries(levels)" :key="item.key">
-				{{ item }}
-			</div>
-			<br>
-		</tbody>
+				<br>
+					<svg id="svgelem" height="500" width="1500">
+						<template v-for="node in renderNodes">
+							<text :x="node.x * 300 + 20" :y="node.y * 100 + 20" fill="red" :key="node.id">{{ node.name }} ({{ node.bday }})</text>
+							<template v-if="node.parents.filter(parent => parent != undefined).length > 0">
+								<line v-for="parentNode in node.parents" :x1="node.x * 300 + 100" :x2="renderNodes.filter(node => node.name === parentNode)[0].x * 300 + 100" :y1="node.y * 100" :y2="renderNodes.filter(node => node.name === parentNode)[0].y * 100 + 30" style="stroke:rgb(255,0,0);stroke-width:2" :key="parentNode.id"></line>
+							</template>
+						</template>
+					</svg>
+				<br>
+			</tbody>
+		</table>
 	</div>
 </template>
 
@@ -29,7 +36,9 @@
 				id: 0,
 				tree:{nodes:[], links:[]},
 				levels: new Map(),
-				xMax: 0
+				xMax: 0,
+				levelsWithX: new Map(),
+				renderNodes: []
 			}
 		},
 		components: {
@@ -54,7 +63,6 @@
 							return procBranch
 						}
 						return procBranch
-
 					})
 					processedBranches.push(...branch.levels.filter(entry => !processedBranches.map(pEntry => pEntry.name).includes(entry.name)))
 					index+=1
@@ -71,7 +79,8 @@
 					index += 1
 				}
 				this.xMax = Math.max(...Object.values(this.levels).map(level => level.length))
-				
+			}).then(() => {
+				return this.calculateXPositions()
 			})
 		},
 		methods: {
@@ -86,12 +95,24 @@
 			},
 			calculateYPositions(entry, returnList, index, parentName){
 				const name = entry.forname + " " + entry.surname
-				returnList.push({name: name, y: index, parents: [parentName]})
+				returnList.push({name: name, bday: entry.bday, y: index, parents: [parentName]})
 				index += 1
 				if(entry.child !== undefined){
 					return this.calculateYPositions(entry.child, returnList, index, name)
 				}
 				return returnList
+			},
+			calculateXPositions(){
+				Object.entries(this.levels).forEach(entry => {
+					this.levels[entry[0]] = this.levels[entry[0]].map((node, index) => {
+						node.x = index
+						return node
+					})
+				})
+				this.levelsWithX = this.levels
+				this.renderNodes = Object.values(this.levelsWithX).flatMap(entry => entry)
+				console.log(this.levelsWithX)
+				console.log(this.renderNodes)
 			},
 			calculateYOffset(branch, processedBranches, index){
 				const indexOf = processedBranches.map(pEntry => pEntry.name).indexOf(branch.forname + " " + branch.surname)
@@ -120,3 +141,15 @@
 		}
 	}
 </script>
+
+<style>
+#tree {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  margin-left: auto;
+  margin-right: auto;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
